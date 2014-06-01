07df1d9ccfd3d89be9bf00f6912b66e2481ca310
librarian <librarian@yandex-team.ru>
2014-06-01 23:46:52 +0400

    Initial commit

---
+++ content/news/039-tvitter-i-novaja-rubrika.md
@@ -0,0 +1,13 @@
+Title: Твиттер и новая рубрика
+Slug: tvitter-i-novaja-rubrika
+Category: Новости проекта
+Date: 2013-03-08 03:06
+Source: False
+Formulas: True
+
+Ряд небольших обновлений:
+
+1. У нас теперь есть твиттер [@whatifrussian](https://twitter.com/whatifrussian) — подписывайтесь, чтобы быть в курсе новых выпусков.
+2. Мы подготовили переводы What If? Numbers [@whatifnumbers](https://twitter.com/whatifnumbers) твиттера с любопытными числами, на которые наткнулся Рэндалл Монро во время своих изысканий. Мы будем их постепенно публиковать, пока не догоним :)
+
+Надеемся, вам понравится.
---
+++ pelicanconf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import logging
+
+AUTHOR = u'whatif'
+SITENAME = u'Что если?'
+SITEURL = 'http://dev.chtoes.li'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = u'ru'
+
+PLUGIN_PATH = "plugins"
+PLUGINS = ["neighbors"]
+THEME = "themes/whatif"
+
+# Feed generation is usually not desired when developing
+FEED_DOMAIN = SITEURL
+FEED_ALL_RSS = "feed/index.xml"
+CATEGORY_FEED_RSS = "feed/category/%s/index.xml"
+TAG_FEED_RSS = "feed/category/%s/index.xml"
+
+# Save as URL
+ARTICLE_URL = 'page/{slug}/'
+ARTICLE_SAVE_AS = 'page/{slug}/index.html'
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_URL = 'tag/'
+TAGS_SAVE_AS = 'tag/index.html'
+
+LOG_FILTER = [
+    (logging.WARN, 'Empty alt attribute for image {} in {}')
+]
+
+
+SLUG_SUBSTITUTIONS = [
+    ("what if?", "what-if"),
+    ("novosti proekta","news"),
+    ("prochee", "other"),
+#("",""),
+]
+
+
+
+DEFAULT_PAGINATION = 1
+
+EXTRA_PATH_METADATA = {
+    'extras/robots.txt': {'path': 'robots.txt'},
+}
+
+STATIC_PATHS = [
+    'uploads',
+    'extras',
+]
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = False
+READERS={'html':None}
+
+
+
