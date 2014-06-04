635ae48497a3d292a6bb65714619cad0e5e07242
Nikita Menkovich <menkovich@gmail.com>
2014-06-05 01:32:07 +0400

    Reworked fabfile, support og meta tags

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,13 @@ env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
 # Remote server configuration
-production = 'chtoes_li@shared.libc6.org:22'
-dest_path = '/var/www/chtoes_li/data/www/dev.chtoes.li/'
 
-
+env.user="chtoes_li"
+env.hosts = ["shared.libc6.org"]
+path = {
+        "dev": '/var/www/chtoes_li/data/www/dev.chtoes.li/',
+        #"prod": '/var/www/chtoes_li/data/www/chtoes.li/'
+        }
 
 def clean():
     if os.path.isdir(DEPLOY_PATH):
@@ -37,11 +40,12 @@ def reserve():
 def preview():
     local('pelican -s publishconf.py')
 
-@hosts(production)
-def publish():
+
+def publish(environment):
+
     local('pelican -q -s pelicanconf.py')
     project.rsync_project(
-        remote_dir=dest_path,
+        remote_dir=path[environment],
         exclude=".DS_Store",
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
         delete=True
