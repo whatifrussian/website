81418673c9dfa0a279eb7429cffc744de1368c5b
Alexander Turenko <totktonada.ru@gmail.com>
2016-08-28 08:20:12 +0300

    Figures: clean up code
    There is one actual output change. Elements after image added inside
    figure AFTER figcaption. It affected footnotes after images and centered
    text in 'Sat Guessing'.

--- md_extensions/figures.py
+++ md_extensions/figures.py
@@ -2,37 +2,57 @@ from markdown.extensions import Extension
 from markdown.treeprocessors import Treeprocessor
 from markdown.util import etree
 
+
+# Utility functions
+# =================
+
+
+def create_figure(img, non_imgs=[]):
+    img.set('class', 'illustration')
+    figure = etree.Element('figure')
+    figure.append(img)
+    em = etree.SubElement(etree.SubElement(
+        etree.SubElement(figure, 'figcaption'), 'div'), 'em')
+    em.text = img.get('title', '')
+    if len(non_imgs) > 0:
+        figure.extend(non_imgs)
+    return figure
+
+
+def replace_element(parent, old, new):
+    idx = list(parent).index(old)
+    parent.remove(old)
+    parent.insert(idx, new)
+    new.text = old.text
+    new.tail = old.tail
+
+
+def wrap_imgs_into_figure(elem):
+    for p in (p for p in elem if p.tag == 'p'):
+        imgs = [img for img in p if img.tag == 'img']
+        if len(imgs) == 1:
+            non_imgs = [e for e in p if e not in imgs]
+            figure = create_figure(imgs[0], non_imgs)
+            replace_element(elem, p, figure)
+        elif len(imgs) > 1:
+            p.tag = 'figure'
+            p.set('class', 'figure_wide')
+            for img in imgs:
+                figure = create_figure(img)
+                replace_element(p, img, figure)
+
+
+# Markdown extension
+# ==================
+
+
 class FiguresTreeprocessor(Treeprocessor):
-    @staticmethod
-    def wrap_imgs_into_figure(elem):
-        def add_figcaption(elem, title):
-            em = etree.SubElement(etree.SubElement(
-                etree.SubElement(elem, 'figcaption'), 'div'), 'em')
-            em.text = title
-
-        for p in (p for p in elem if p.tag == 'p'):
-            imgs = [img for img in p if img.tag == 'img']
-            if len(imgs) == 1:
-                p.tag = 'figure'
-                img = imgs[0]
-                add_figcaption(p, img.get('title', ''))
-                img.set('class', 'illustration')
-            elif len(imgs) > 1:
-                p.tag = 'figure'
-                p.set('class', 'figure_wide')
-                for img in imgs:
-                    figure = etree.SubElement(p, 'figure')
-                    p.remove(img)
-                    figure.append(img)
-                    add_figcaption(figure, img.get('title', ''))
-                    img.set('class', 'illustration')
-
     def run(self, root):
         # wrap images in footnotes
         for li in root.findall("div[@class='footnote']/ol/li"):
-            FiguresTreeprocessor.wrap_imgs_into_figure(li)
+            wrap_imgs_into_figure(li)
         # wrap images in an article
-        FiguresTreeprocessor.wrap_imgs_into_figure(root)
+        wrap_imgs_into_figure(root)
 
 class FiguresExtension(Extension):
     def extendMarkdown(self, md, md_globals):
