0194a92b14c7e92d808ec46b1ebf953289e5344e
Alexander Turenko <totktonada.ru@gmail.com>
2016-09-07 17:14:08 +0300

    New '>>' syntax for questions
    Useful when an article has several questions. The first '>' quote become
    question as it was.

--- md_extensions/question.py
+++ md_extensions/question.py
@@ -1,25 +1,51 @@
+import re
 from markdown.extensions import Extension
 from markdown.treeprocessors import Treeprocessor
 from markdown.util import etree
+from markdown.blockprocessors import BlockQuoteProcessor
+
+
+# Utility functions
+# =================
+
+
+def tweak_question(blockquote):
+    blockquote.set('class', 'question')
+    div = etree.Element('div')
+    for child in list(blockquote):
+        div.append(child)
+        blockquote.remove(child)
+    blockquote.append(div)
 
 
 # Markdown extension
 # ==================
 
 
+# Parse special question blockquote (>>).
+class QuestionBlockProcessor(BlockQuoteProcessor):
+    RE = re.compile(r'(^|\n)[ ]{0,3}>>[ ]?(.*)')
+
+    def run(self, parent, blocks):
+        super(QuestionBlockProcessor, self).run(parent, blocks)
+        blockquote = self.lastChild(parent)
+        tweak_question(blockquote)
+
+
+# Fix first regular blockquote (>).
 class QuestionTreeprocessor(Treeprocessor):
     def run(self, root):
-        blockquote = root.find("blockquote")
+        blockquote = root.find('blockquote')
         if blockquote is None:
             return None
-        blockquote.set('class', 'question')
-        div = etree.Element('div')
-        for child in list(blockquote):
-            div.append(child)
-            blockquote.remove(child)
-        blockquote.append(div)
+        if blockquote.get('class') == 'question':
+            # It's a blockquote created by QuestionBlockProcessor
+            return None
+        tweak_question(blockquote)
 
 
 class QuestionExtension(Extension):
     def extendMarkdown(self, md, md_globals):
+        md.parser.blockprocessors.add(
+            'question', QuestionBlockProcessor(md.parser), '<quote')
         md.treeprocessors['question'] = QuestionTreeprocessor()
