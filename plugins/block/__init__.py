f2074e175b3392bf6b14a3fb6284dfe5922a0daf
Nikita Menkovich <menkovich@gmail.com>
2025-05-20 01:47:11 +0200

    forgot file?

--- plugins/block/__init__.py
+++ plugins/block/__init__.py
@@ -1,62 +1,45 @@
 from pelican import signals
+from pelican.contents import Article
+from pelican.generators import ArticlesGenerator
+from pelican.writers import Writer
 import os
+from copy import deepcopy
 import logging
 import markdown
 
 logger = logging.getLogger(__name__)
 
-def patch_blocked_override(generator):
-    generator.blocked_variants = []
+PREFIX = "[BLOCK]"
 
-    for article in list(generator.articles):
+def write_blocked_articles(generator: ArticlesGenerator, writer: Writer):
+    for article in generator.articles:
         blocked_country = article.metadata.get('blocked_country')
         blocked_content = article.metadata.get('blocked_content_override')
-
-        logger.debug("Processing article %s", article.slug)
-        logger.debug("Article metadata: %s", article.metadata)
+        logger.debug("%s Processing article %s", PREFIX, article.slug)
+        logger.debug("%s Article metadata: %s", PREFIX, article.metadata)
 
         if not blocked_country or not blocked_content:
-            logger.debug("Article %s does not have blocked_country or blocked_content_override metadata.", article.title)
+            logger.debug("%s No blocked country or content override found for %s", PREFIX, article.slug)
             continue
-
-        logger.debug("Article %s has blocked_country: %s", article.title, blocked_country)
-
+        
         if not blocked_content.strip().startswith("<"):
             blocked_content = markdown.markdown(blocked_content)
 
-        article.blocked_country = blocked_country
-        article.blocked_content_override = blocked_content
-        generator.blocked_variants.append(article)
+        logger.debug("%s Writing blocked content for %s: %s", PREFIX, article.slug, blocked_content)
 
-    logger.info("Found %d articles with blocked_country override", len(generator.blocked_variants))
-
-
-def write_blocked_override(generator, writer):
-    for article in getattr(generator, 'blocked_variants', []):
-        blocked_country = article.blocked_country
-        blocked_content = article.blocked_content_override
-
-        if not blocked_content.strip().startswith("<"):
-            blocked_content = markdown.markdown(blocked_content)
-
-        logger.debug("Writing blocked_country variant for %s", article.slug)
-        logger.debug("Blocked content: %s", blocked_content)
-
-        # Derive path: 'slug/index_{blocked_country}.html'
         base_dir = os.path.dirname(article.save_as)
-        new_filename = "index_{blocked_country}.html".format(blocked_country=blocked_country)
+        new_filename = "index_{}.html".format(blocked_country)
         alt_save_as = os.path.join(base_dir, new_filename)
         alt_url = os.path.join(os.path.dirname(article.url), new_filename)
 
-        logger.info("Writing blocked_country variant for %s to %s", article.slug, alt_save_as)
-
         context = generator.context.copy()
-
         context.update({
             'article': article,
             'content': blocked_content,
             'blocked_country': blocked_country,
         })
+
+        article.blocked_content = blocked_content
 
         writer.write_file(
             alt_save_as,
@@ -69,8 +52,10 @@ def write_blocked_override(generator, writer):
             url=alt_url,
             save_as=alt_save_as,
         )
+        
 
+
+        
 
 def register():
-    signals.article_generator_finalized.connect(patch_blocked_override)
-    signals.article_writer_finalized.connect(write_blocked_override)
+    signals.article_writer_finalized.connect(write_blocked_articles)
